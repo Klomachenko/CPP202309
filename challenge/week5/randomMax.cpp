90fb64f0e35aa86bb1e54a68bc80b0a082e96960
Klomachenko <leekyumin0901@naver.com>
2023-10-04 14:29:16 +0900

    Week5 : 범위기반 이중 for문 구현

--- challenge/week5/randomMax.cpp
+++ challenge/week5/randomMax.cpp
@@ -21,21 +21,23 @@ int main() {
     }
 
     // 최댓값과 해당 위치 찾기
-    // 정수 변수 선언
-    // max 즉 최댓값을 [0][0]이라는 첫째 위치로 해주면 탐색이 더 편하지 않을까? 하여 코드 삽입
-    // 완전탐색 알고리즘을 생각하고 코드를 작성해보자
-    int max = numList[0][0];  
-    int maxI;  
-    int maxJ;
+    int max = numList[0][0];
+    int maxI = 0;
+    int maxJ = 0;
 
-    // 이중for문 이용 모든 수를 돌며 위치를 찾는다
+    // 이중 for문 이용 모든 수를 돌며 위치를 찾는다
     for (int i = 0; i < numCell; ++i) {
-        for (int j = 0; j < numCell; ++j) {
-            if (numList[i][j] > max) {
-                max = numList[i][j];
-                maxI = i;
-                maxJ = j;
+        int j = 0;  // 변수 j를 0으로 초기화 j는 열의 인덱
+        // 범위기반 for 문을 이용하여 각 행의 요소에 접근
+        for (int currentNum : numList[i]) {
+            // 각 행의 요소를 currentNum 변수에 순차적으로 할당하여 접근
+            // 만약 현재 요소가 최댓값인 max보다 크다면 아래를 실행
+            if (currentNum > max) {
+                max = currentNum;  // 최댓값을 갱신
+                maxI = i;  // 최댓값의 행 위치를 i로 갱신
+                maxJ = j;  // 최댓값의 열 위치를 j로 갱신
             }
+            j++;  // 열 인덱스 j를 증가시켜 다음 열의 요소로 이동
         }
     }
 
